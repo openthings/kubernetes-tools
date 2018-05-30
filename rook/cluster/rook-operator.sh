fbf039afb4604eabe949d23a321920ab73dea949
openthings <openthings@163.com>
2018-05-30 14:35:09 +0800

    update daily.

---
+++ rook/cluster/rook-operator.sh
@@ -0,0 +1,1 @@
+kubectl apply -f  /home/alex/Go/src/rook/cluster/examples/kubernetes/rook-operator.yaml
---
+++ rook/helm-rook-operator.sh
@@ -0,0 +1,2 @@
+helm repo add rook-alpha https://charts.rook.io/alpha
+helm install rook-alpha/rook
