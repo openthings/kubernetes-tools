1b6410728a13d4b35e93c9df3dc07922c585a2f7
openthings <openthings@163.com>
2018-07-11 20:43:06 +0800

    update daily.

--- kubeflow/kubeflow-images-tool/b1-kubeflow-images-worker-pullfromgcr1.8.sh
+++ kubeflow/kubeflow-images-tool/b1-kubeflow-images-worker-pullfromgcr1.8.sh
@@ -1,7 +1,6 @@
 MY_REGISTRY=gcr.io/kubeflow-images-public
-MY_IMAGE_CPU=tensorflow-1.7.0-notebook-cpu:v20180711-4c0b6a5a
-MY_IMAGE_GPU=tensorflow-1.7.0-notebook-gpu:v20180710-bf1c2f0a
-
+MY_IMAGE_CPU=tensorflow-1.8.0-notebook-cpu:v20180711-4c0b6a5a
+MY_IMAGE_GPU=tensorflow-1.8.0-notebook-gpu:v20180710-bf1c2f0a
 
 echo ""
 
