96ee8a5eab92e2dedb17ada03d4d6824f283b908
openthings <openthings@163.com>
2018-08-15 21:56:16 +0800

    add ingress images tool.

--- ingress/nginx-ingress-install2.sh
+++ ingress/nginx-ingress-install2.sh
@@ -5,7 +5,7 @@ echo "Please visit for more parameters: "
 echo "	https://github.com/kubernetes/charts/tree/master/stable/nginx-ingress#configuration"
 echo ""
 
-helm install stable/nginx-ingress --name nginx-ingress \
+helm install ./nginx-ingress --name nginx-ingress \
 	--namespace kube-system \
 	--set controller.stats.enabled=true
 
