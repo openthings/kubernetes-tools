77cbed974303997033b2591f9044ddfcf8341805
openthings <openthings@163.com>
2018-08-24 11:00:20 +0800

    update kubeadm/...

--- kubeadm/2-docker-image-tool/extra-pull-gcr.sh
+++
@@ -1,2 +0,0 @@
-docker pull k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
-docker pull gcr.io/kubernetes-helm/tiller:v2.9.1
--- kubeadm/kubeadm-install-1.10.7.sh
+++ kubeadm/kubeadm-install-1.10.7.sh
@@ -2,7 +2,7 @@ echo "添加Kubernetes安装源认证key:"
 sudo curl -sSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 
 echo "添加Kubernetes安装源："
-sudo echo “deb http://apt.kubernetes.io/ kubernetes-xenial main” > /etc/apt/sources.list.d/kubernetes.list
+sudo echo deb http://apt.kubernetes.io/ kubernetes-xenial main > /etc/apt/sources.list.d/kubernetes.list
 
 echo "更新系统软件包列表："
 sudo apt update
@@ -10,5 +10,5 @@ sudo apt update
 echo "查看Kubernetes的可用版本："
 apt-cache madison kubeadm
 
-echo "安装kubeadm 1.9.3: "
-apt-get install -y kubeadm=1.9.3-00
+echo "安装kubeadm 1.10.7: "
+apt-get install -y kubeadm=1.10.7-00
