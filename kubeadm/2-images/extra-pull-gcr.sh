f19b917292849ea02b7ab969d284d3ee10362afa
openthings <openthings@163.com>
2018-08-24 17:36:16 +0800

    add ingress image tool to extra

--- kubeadm/2-images/extra-pull-gcr.sh
+++ kubeadm/2-images/extra-pull-gcr.sh
@@ -1,4 +1,12 @@
-docker pull quay.io/coreos/flannel:v0.10.0-amd64
-docker pull gcr.io/kubernetes-helm/tiller:v2.9.1
+docker pull k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
 docker pull k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0
+
+docker pull gcr.io/kubernetes-helm/tiller:v2.9.1
+
+docker pull quay.io/coreos/flannel:v0.10.0-amd64
+
+# For Ingress
+docker pull quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.14.0
+docker pull quay.io/k8s.gcr.io/defaultbackend:1.3
+
 
