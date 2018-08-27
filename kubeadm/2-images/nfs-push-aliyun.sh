d36e393d65fccc4a82b92012a318bcb6be1c4ccc
openthings <openthings@163.com>
2018-08-27 11:26:23 +0800

    update nfs provisioner to quay.io/kubernetes_incubator/nfs-provisioner:v2.0.1-k8s1.11

--- kubeadm/2-images/nfs-push-aliyun.sh
+++ kubeadm/2-images/nfs-push-aliyun.sh
@@ -1,5 +1,5 @@
 
 MY_REGISTRY=registry.cn-hangzhou.aliyuncs.com/openthings
 
-docker tag quay.io/kubernetes_incubator/nfs-provisioner:v1.0.9 ${MY_REGISTRY}/quay-io-nfs-provisioner:v1.0.9
-docker push ${MY_REGISTRY}/quay-io-nfs-provisioner:v1.0.9
+docker tag quay.io/kubernetes_incubator/nfs-provisioner:v2.0.1-k8s1.11 ${MY_REGISTRY}/quay-io-nfs-provisioner:v2.0.1-k8s1.11
+docker push ${MY_REGISTRY}/quay-io-nfs-provisioner:v2.0.1-k8s1.11
