37e9dc857710f9813b52fa120263c2e3082041f4
openthings <openthings@163.com>
2019-01-04 18:45:39 +0800

    update

--- kubeadm/2-images/kubernetes-pull-aliyun-1.13.1.sh
+++ kubeadm/2-images/kubernetes-pull-aliyun-1.13.1.sh
@@ -23,7 +23,7 @@ docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.13.1 k8s.gcr.io/
 docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.13.1 k8s.gcr.io/kube-proxy:v1.13.1
 docker tag ${MY_REGISTRY}/k8s-gcr-io-etcd:3.2.24 k8s.gcr.io/etcd:3.2.24
 docker tag ${MY_REGISTRY}/k8s-gcr-io-pause:3.1 k8s.gcr.io/pause:3.1
-docker tag ${MY_REGISTRY}/k8s-gcr-io-coredns:1.2.2 k8s.gcr.io/coredns:1.2.6
+docker tag ${MY_REGISTRY}/k8s-gcr-io-coredns:1.2.6 k8s.gcr.io/coredns:1.2.6
 
 echo ""
 echo "=========================================================="
