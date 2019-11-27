8d353abd6e722a70b05fa856b87c162998c21e0f
openthings <openthings@163.com>
2019-11-27 20:09:46 +0800

    update k8s to 1.16.3

--- kubeadm/2-images/kubernetes-push-aliyun-1.16.3.sh
+++ kubeadm/2-images/kubernetes-push-aliyun-1.16.3.sh
@@ -1,6 +1,6 @@
 echo ""
 echo "=========================================================="
-echo "Push Kubernetes 1.14.1 Images into aliyuncs.com ......"
+echo "Push Kubernetes v1.16.3 Images into aliyuncs.com ......"
 echo "=========================================================="
 echo ""
 
@@ -10,13 +10,13 @@ echo "docker tag to openthings ..."
 
 MY_REGISTRY=registry.cn-hangzhou.aliyuncs.com/openthings
 
-docker tag k8s.gcr.io/kube-apiserver:v1.14.1 ${MY_REGISTRY}/k8s-gcr-io-kube-apiserver:v1.14.1
-docker tag k8s.gcr.io/kube-scheduler:v1.14.1 ${MY_REGISTRY}/k8s-gcr-io-kube-scheduler:v1.14.1
-docker tag k8s.gcr.io/kube-controller-manager:v1.14.1 ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.14.1
-docker tag k8s.gcr.io/kube-proxy:v1.14.1 ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.14.1
-docker tag k8s.gcr.io/etcd:3.3.10 ${MY_REGISTRY}/k8s-gcr-io-etcd:3.3.10 
+docker tag k8s.gcr.io/kube-apiserver:v1.16.3 ${MY_REGISTRY}/k8s-gcr-io-kube-apiserver:v1.16.3
+docker tag k8s.gcr.io/kube-scheduler:v1.16.3 ${MY_REGISTRY}/k8s-gcr-io-kube-scheduler:v1.16.3
+docker tag k8s.gcr.io/kube-controller-manager:v1.16.3 ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.16.3
+docker tag k8s.gcr.io/kube-proxy:v1.16.3 ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.16.3
+docker tag k8s.gcr.io/etcd:3.3.15-0 ${MY_REGISTRY}/k8s-gcr-io-etcd:3.3.15-0 
 docker tag k8s.gcr.io/pause:3.1 ${MY_REGISTRY}/k8s-gcr-io-pause:3.1
-docker tag k8s.gcr.io/coredns:1.3.1 ${MY_REGISTRY}/k8s-gcr-io-coredns:1.3.1
+docker tag k8s.gcr.io/coredns:1.6.2 ${MY_REGISTRY}/k8s-gcr-io-coredns:1.6.2
 
 echo ""
 echo "=========================================================="
@@ -25,23 +25,23 @@ echo ""
 ## Push镜像
 echo ""
 echo "1.k8s-kube-apiserver"
-docker push ${MY_REGISTRY}/k8s-gcr-io-kube-apiserver:v1.14.1
+docker push ${MY_REGISTRY}/k8s-gcr-io-kube-apiserver:v1.16.3
 
 echo ""
 echo "2.k8s-kube-controller-manager"
-docker push ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.14.1
+docker push ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.16.3
 
 echo ""
 echo "3.k8s-kube-scheduler"
-docker push ${MY_REGISTRY}/k8s-gcr-io-kube-scheduler:v1.14.1
+docker push ${MY_REGISTRY}/k8s-gcr-io-kube-scheduler:v1.16.3
 
 echo ""
 echo "4.k8s-kube-proxy"
-docker push ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.14.1
+docker push ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.16.3
 
 echo ""
 echo "5.k8s-etcd"
-docker push ${MY_REGISTRY}/k8s-gcr-io-etcd:3.3.10
+docker push ${MY_REGISTRY}/k8s-gcr-io-etcd:3.3.15-0
 
 echo ""
 echo "6.k8s-pause"
@@ -49,11 +49,11 @@ docker push ${MY_REGISTRY}/k8s-gcr-io-pause:3.1
 
 echo ""
 echo "7.k8s-coredns"
-docker push ${MY_REGISTRY}/k8s-gcr-io-coredns:1.3.1
+docker push ${MY_REGISTRY}/k8s-gcr-io-coredns:1.6.2
 
 echo ""
 echo "=========================================================="
-echo "Push Kubernetes 1.14.1 Images FINISHED."
+echo "Push Kubernetes v1.16.3 Images FINISHED."
 echo "into registry.cn-hangzhou.aliyuncs.com/openthings, "
 echo "           by openthings@https://my.oschina.net/u/2306127."
 echo "=========================================================="
