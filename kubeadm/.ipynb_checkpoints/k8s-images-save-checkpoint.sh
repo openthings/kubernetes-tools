e6edfb4d1bbd0e017e56a2b71d7102782da9c596
openthings <openthings@163.com>
2020-01-17 11:37:57 +0800

    update k8s-images tools

--- kubeadm/.ipynb_checkpoints/k8s-images-save-checkpoint.sh
+++ kubeadm/.ipynb_checkpoints/k8s-images-save-checkpoint.sh
@@ -7,7 +7,7 @@ echo "###################################################################"
 echo "Kubernetes core system images."
 echo "-------------------------------------------------------------------"
 
-K8S_VERSION="1.16.3"
+K8S_VERSION="1.17.1"
 
 IMAGES_PATH=~/k8s-images-$K8S_VERSION
 
@@ -33,12 +33,12 @@ echo "A5.>> k8s.gcr.io/pause:3.1"
 docker save k8s.gcr.io/pause:3.1 -o $IMAGES_PATH/A5-k8s-pause-3.1.tar
 echo ""
 
-echo "A6.>> k8s.gcr.io/etcd:3.3.15-0"
-docker save k8s.gcr.io/etcd:3.3.15-0 -o $IMAGES_PATH/A6-k8s-etcd-3.3.15-0.tar
+echo "A6.>> k8s.gcr.io/etcd:3.4.3-0"
+docker save k8s.gcr.io/etcd:3.4.3-0 -o $IMAGES_PATH/A6-k8s-etcd-3.4.3-0.tar
 echo ""
 
-echo "A7.>> k8s.gcr.io/coredns:1.6.2"
-docker save k8s.gcr.io/coredns:1.6.2 -o $IMAGES_PATH/A7-k8s-coredns-1.6.2.tar
+echo "A7.>> k8s.gcr.io/coredns:1.6.5"
+docker save k8s.gcr.io/coredns:1.6.5 -o $IMAGES_PATH/A7-k8s-coredns-1.6.5.tar
 echo ""
 
 echo "==================================================================="
