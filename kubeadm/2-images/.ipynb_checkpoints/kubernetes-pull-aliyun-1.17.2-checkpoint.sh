eeefbca7daa11ff60ae3012fa598b41ebbd3d8b2
openthings <openthings@163.com>
2020-02-29 23:24:53 +0800

    update k8s images 1.17.3

--- kubeadm/2-images/.ipynb_checkpoints/kubernetes-pull-aliyun-1.17.2-checkpoint.sh
+++ kubeadm/2-images/.ipynb_checkpoints/kubernetes-pull-aliyun-1.17.2-checkpoint.sh
@@ -17,10 +17,10 @@ docker pull ${MY_REGISTRY}/k8s-gcr-io-coredns:1.6.5
 
 
 ## 添加Tag
-docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-apiserver:v1.17.1 k8s.gcr.io/kube-apiserver:v1.17.2
-docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-scheduler:v1.17.1 k8s.gcr.io/kube-scheduler:v1.17.2
-docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.17.1 k8s.gcr.io/kube-controller-manager:v1.17.2
-docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.17.1 k8s.gcr.io/kube-proxy:v1.17.2
+docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-apiserver:v1.17.2 k8s.gcr.io/kube-apiserver:v1.17.2
+docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-scheduler:v1.17.2 k8s.gcr.io/kube-scheduler:v1.17.2
+docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-controller-manager:v1.17.2 k8s.gcr.io/kube-controller-manager:v1.17.2
+docker tag ${MY_REGISTRY}/k8s-gcr-io-kube-proxy:v1.17.2 k8s.gcr.io/kube-proxy:v1.17.2
 docker tag ${MY_REGISTRY}/k8s-gcr-io-etcd:3.4.3-0 k8s.gcr.io/etcd:3.4.3-0
 docker tag ${MY_REGISTRY}/k8s-gcr-io-pause:3.1 k8s.gcr.io/pause:3.1
 docker tag ${MY_REGISTRY}/k8s-gcr-io-coredns:1.6.5 k8s.gcr.io/coredns:1.6.5
