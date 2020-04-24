30795e02d2fa5f8ce0bf9f94e89cf0a38d4061c4
openthings <openthings@163.com>
2020-04-24 08:37:37 +0800

    update images 1.18.2

--- kubeadm/2-images/.ipynb_checkpoints/kubernetes-pull-hub-arm64-1.18.1-checkpoint.sh
+++ kubeadm/2-images/.ipynb_checkpoints/kubernetes-pull-hub-arm64-1.18.1-checkpoint.sh
@@ -1,16 +1,19 @@
+MY_REGISTRY=mirrorgcrio
+K8S_VERSION="1.18.2"
+
 echo ""
 echo "=========================================================="
-echo "Pull Kubernetes for arm v1.18.1 Images from docker.io ......"
+echo "Pull Kubernetes for arm v$K8S_VERSION Images from docker.io ......"
 echo "=========================================================="
 echo ""
 
 MY_REGISTRY=mirrorgcrio
 
 ## 拉取镜像
-docker pull ${MY_REGISTRY}/kube-apiserver-arm64:v1.18.1
-docker pull ${MY_REGISTRY}/kube-controller-manager-arm64:v1.18.1
-docker pull ${MY_REGISTRY}/kube-scheduler-arm64:v1.18.1
-docker pull ${MY_REGISTRY}/kube-proxy-arm64:v1.18.1
+docker pull ${MY_REGISTRY}/kube-apiserver-arm64:v$K8S_VERSION
+docker pull ${MY_REGISTRY}/kube-controller-manager-arm64:v$K8S_VERSION
+docker pull ${MY_REGISTRY}/kube-scheduler-arm64:v$K8S_VERSION
+docker pull ${MY_REGISTRY}/kube-proxy-arm64:v$K8S_VERSION
 docker pull ${MY_REGISTRY}/etcd-arm64:3.4.3-0
 docker pull ${MY_REGISTRY}/pause-arm64:3.2
 
@@ -18,10 +21,10 @@ docker pull ${MY_REGISTRY}/pause-arm64:3.2
 docker pull coredns/coredns:1.6.7
 
 ## 添加Tag
-docker tag ${MY_REGISTRY}/kube-apiserver-arm64:v1.18.1 k8s.gcr.io/kube-apiserver:v1.18.1
-docker tag ${MY_REGISTRY}/kube-scheduler-arm64:v1.18.1 k8s.gcr.io/kube-scheduler:v1.18.1
-docker tag ${MY_REGISTRY}/kube-controller-manager-arm64:v1.18.1 k8s.gcr.io/kube-controller-manager:v1.18.1
-docker tag ${MY_REGISTRY}/kube-proxy-arm64:v1.18.1 k8s.gcr.io/kube-proxy:v1.18.1
+docker tag ${MY_REGISTRY}/kube-apiserver-arm64:v$K8S_VERSION k8s.gcr.io/kube-apiserver:v$K8S_VERSION
+docker tag ${MY_REGISTRY}/kube-scheduler-arm64:v$K8S_VERSION k8s.gcr.io/kube-scheduler:v$K8S_VERSION
+docker tag ${MY_REGISTRY}/kube-controller-manager-arm64:v$K8S_VERSION k8s.gcr.io/kube-controller-manager:v$K8S_VERSION
+docker tag ${MY_REGISTRY}/kube-proxy-arm64:v$K8S_VERSION k8s.gcr.io/kube-proxy:v$K8S_VERSION
 docker tag ${MY_REGISTRY}/etcd-arm64:3.4.3-0 k8s.gcr.io/etcd:3.4.3-0
 docker tag ${MY_REGISTRY}/pause-arm64:3.2 k8s.gcr.io/pause:3.2
 
@@ -30,7 +33,7 @@ docker tag coredns/coredns:1.6.7 k8s.gcr.io/coredns:1.6.7
 
 echo ""
 echo "=========================================================="
-echo "Pull Kubernetes for arm v1.18.1 Images FINISHED."
+echo "Pull Kubernetes for arm v$K8S_VERSION Images FINISHED."
 echo "into docker.io/mirrorgcrio, "
 echo " by openthings@https://my.oschina.net/u/2306127."
 echo "=========================================================="
