aa35f294ac15d434f164d46ee703a944148893f0
openthings <openthings@163.com>
2020-12-17 22:12:24 +0800

    update k8s-arm 1.20.0

--- kubeadm/2-images/.ipynb_checkpoints/kubernetes-pull-hub-arm64-checkpoint.sh
+++ kubeadm/2-images/.ipynb_checkpoints/kubernetes-pull-hub-arm64-checkpoint.sh
@@ -1,4 +1,5 @@
-MY_REGISTRY=mirrorgcrio
+#MY_REGISTRY=mirrorgcrio
+MY_REGISTRY=registry.cn-hangzhou.aliyuncs.com/google_containers
 K8S_VERSION="1.20.0"
 
 echo ""
@@ -14,22 +15,22 @@ docker pull ${MY_REGISTRY}/kube-apiserver-arm64:v$K8S_VERSION
 docker pull ${MY_REGISTRY}/kube-controller-manager-arm64:v$K8S_VERSION
 docker pull ${MY_REGISTRY}/kube-scheduler-arm64:v$K8S_VERSION
 docker pull ${MY_REGISTRY}/kube-proxy-arm64:v$K8S_VERSION
-docker pull ${MY_REGISTRY}/etcd-arm64:3.4.3-0
+docker pull ${MY_REGISTRY}/etcd-arm64:3.4.13-0
 docker pull ${MY_REGISTRY}/pause-arm64:3.2
 
 #docker pull ${MY_REGISTRY}/coredns-arm64:1.6.7
-docker pull coredns/coredns:1.6.7
+docker pull coredns/coredns:1.7.0
 
 ## 添加Tag
 docker tag ${MY_REGISTRY}/kube-apiserver-arm64:v$K8S_VERSION k8s.gcr.io/kube-apiserver:v$K8S_VERSION
 docker tag ${MY_REGISTRY}/kube-scheduler-arm64:v$K8S_VERSION k8s.gcr.io/kube-scheduler:v$K8S_VERSION
 docker tag ${MY_REGISTRY}/kube-controller-manager-arm64:v$K8S_VERSION k8s.gcr.io/kube-controller-manager:v$K8S_VERSION
 docker tag ${MY_REGISTRY}/kube-proxy-arm64:v$K8S_VERSION k8s.gcr.io/kube-proxy:v$K8S_VERSION
-docker tag ${MY_REGISTRY}/etcd-arm64:3.4.3-0 k8s.gcr.io/etcd:3.4.3-0
+docker tag ${MY_REGISTRY}/etcd-arm64:3.4.13-0 k8s.gcr.io/etcd:3.4.13-0
 docker tag ${MY_REGISTRY}/pause-arm64:3.2 k8s.gcr.io/pause:3.2
 
 #docker tag ${MY_REGISTRY}/coredns-arm64:1.6.7 k8s.gcr.io/coredns:1.6.7
-docker tag coredns/coredns:1.6.7 k8s.gcr.io/coredns:1.6.7
+docker tag coredns/coredns:1.7.0 k8s.gcr.io/coredns:1.7.0
 
 echo ""
 echo "=========================================================="
